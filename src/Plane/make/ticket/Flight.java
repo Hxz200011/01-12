6997b786a54149bc476556c6d7b63396a4d439c5
Administrator <@2843106425>
2021-01-12 09:38:45 +0800

    1.类，对象 2.多态，接口，抽象 3.飞机订票系统

---
+++ src/Plane/make/ticket/Flight.java
@@ -0,0 +1,49 @@
+package Plane.make.ticket;
+
+public class Flight {
+    private String id;
+    private String planeType;//飞机型号
+    private String departureAirport;//飞机出发点
+    private String destinationAirPort;//飞机到达的目的地
+    private String departureTime;//飞机的飞行时长
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPlaneType() {
+        return planeType;
+    }
+
+    public void setPlaneType(String planeType) {
+        this.planeType = planeType;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDestinationAirPort() {
+        return destinationAirPort;
+    }
+
+    public void setDestinationAirPort(String destinationAirPort) {
+        this.destinationAirPort = destinationAirPort;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+}
